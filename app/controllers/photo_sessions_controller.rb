f15e4f58dcb60bdab78642df5a080086e31588ef
Adam Gamble <adamgamble@gmail.com>
2014-02-16 21:11:38 -0600

    It works!

--- app/controllers/photo_sessions_controller.rb
+++ app/controllers/photo_sessions_controller.rb
@@ -2,6 +2,15 @@ class PhotoSessionsController < ApplicationController
   def show
     @photo_session = PhotoSession.find(params[:id])
 
-    render :pdf => "photo_session_#{params[:id]}.pdf", :layout => false
+    html = render_to_string
+    save_path = "#{Rails.root}/tmp/photo_session_#{params[:id]}.html"
+    pdf_save_path = "#{Rails.root}/tmp/photo_session_#{params[:id]}.pdf"
+    File.open(save_path, 'wb') do |file|
+        file << html
+    end
+    `wkhtmltopdf --page-width 5in --page-height 7in --margin-left 0 --margin-right 0 --margin-top 0 --margin-bottom 0 #{save_path} #{pdf_save_path}`
+    `lpr #{pdf_save_path}`
+    render :pdf => "photo_session_#{params[:id]}.pdf", :layout => false,
+      :margin => {:top => 0, :left => 0, :right => 0, :bottom => 0}, :extra => "--page-height 7in --page-width 5in"
   end
 end
